5cfe78bfb3f8c5a0a3acecf796e32b02e4fa0c8d
shreyas_dixit <shreyasdixit62442@gmail.com>
2020-07-08 18:24:29 +0530

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,7 +137,7 @@ BOOTSTRAP3 = { 'include_jquery': True,
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES ={
-        'default': dj_database_url.config(default='postgress://localhost')
+        'default': dj_database_url.config(default='postgres://localhost')
         }
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
